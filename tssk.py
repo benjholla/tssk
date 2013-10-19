a37fcc92482ca717c045d5b0585fa1bb3041de71
benjholla <benjholla@gmail.com>
2013-10-19 18:28:23 -0500

    added usuage help

--- tssk.py
+++ tssk.py
@@ -3,16 +3,20 @@ import getopt
 import subprocess
 import StringIO
 
+version = "0.1"
+
 def main(argv):
+	
 	# set option defaults
 	tshark = "tshark" # uses system variable as default tshark location
 	deviceID = None
 	deviceName = None
+	pcap = None
 	debug = False
 	
 	# parse command line options
 	try:
-		opts, args = getopt.getopt(argv, "ht:d", ["help", "tshark=", "device="])
+		opts, args = getopt.getopt(argv, "hvd", ["help", "version", "tshark=", "device=", "pcap="])
 	except getopt.GetoptError:
 		usage()
 		sys.exit(2)
@@ -20,10 +24,15 @@ def main(argv):
 		if opt in ("-h", "--help"):
 			usage()
 			sys.exit()
+		if opt in ("-v", "--version"):
+			print "tssk version " + version
+			sys.exit()
 		elif opt == '-d':
 			debug = True
-		elif opt in ("-t", "--tshark"):
+		elif opt in ("--tshark"):
 			tshark = arg
+		elif opt in ("--pcap"):
+			pcap = arg
 		elif opt in ("--device"):
 			try:
 				deviceID = int(arg)
@@ -35,27 +44,46 @@ def main(argv):
 	if debug:
 		print "tshark: " + tshark
 	
-	# make sure we have a valid capture device
-	out,err = subprocess.Popen(['tshark', '-D'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-	deviceList = (err if out == "" else out).strip()
-	if not deviceID and not deviceName:
-		print "Select a capture device with '--device=' argument:"
-		print deviceList
+	# if not parsing a pcap file make sure we have a valid capture device
+	if not pcap:
+		out,err = subprocess.Popen(['tshark', '-D'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+		deviceList = (err if out == "" else out).strip()
+		if not deviceID and not deviceName:
+			print "Specify a pcap file to parse with '--pcap=' argument or select a capture device with '--device=' argument:"
+			print deviceList
+			sys.exit()
+		else:
+			for line in StringIO.StringIO(deviceList).readlines():
+				deviceEntry = line.split(" ")
+				deviceEntryID = int(deviceEntry[0].replace(".","").strip())
+				deviceEntryName = deviceEntry[1].strip()
+				if(deviceName == deviceEntryName):
+					break
+				if(deviceID == deviceEntryID):
+					deviceName = deviceEntryName
+					break
+			if debug:
+				print "device: " + deviceName
+			# start capturing traffic on selected device
 	else:
-		for line in StringIO.StringIO(deviceList).readlines():
-			deviceEntry = line.split(" ")
-			deviceEntryID = int(deviceEntry[0].replace(".","").strip())
-			deviceEntryName = deviceEntry[1].strip()
-			if(deviceName == deviceEntryName):
-				break
-			if(deviceID == deviceEntryID):
-				deviceName = deviceEntryName
-				break
-		if debug:
-			print "device: " + deviceName
+		# start parsing the pcap file for sessions
+		try:
+			open(pcap) # ensure that the file exists
+			
+		except IOError:
+			print "The specified pcap file does not exist."
 
 def usage():
-	print "TODO"
+	print "Usage: tssk [options] ..."
+	print "Configuration:"
+	print "  --tshark                 sets the path to tshark"
+	print "Processing:"
+	print "  --pcap                   sets the path to the pcap file to parse"
+	print "  --device <id or name>    sets the device on which to capture traffic"
+	print "Miscellaneous:"
+	print "  -h                       display this help and exit"
+	print "  -v                       display version info and exit"
+	print "  -d                       enable debug messages"
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
